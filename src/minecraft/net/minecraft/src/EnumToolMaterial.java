16555721d8d849841bc742e08a9e909fa645b5ef
BuildTools <unconfigured@null.spigotmc.org>
2024-04-30 20:46:08 +0200

    feat(tiers): added netherite tier

--- src/minecraft/net/minecraft/src/EnumToolMaterial.java
+++ src/minecraft/net/minecraft/src/EnumToolMaterial.java
@@ -6,7 +6,8 @@ public enum EnumToolMaterial
     STONE(1, 131, 4.0F, 1.0F, 5),
     IRON(2, 250, 6.0F, 2.0F, 14),
     EMERALD(3, 1561, 8.0F, 3.0F, 10),
-    GOLD(0, 32, 12.0F, 0.0F, 22);
+    GOLD(0, 32, 12.0F, 0.0F, 22),
+    NETHERITE(4, 2032, 9, 4, 15);
 
     /**
      * The level of material this tool can harvest (3 = DIAMOND, 2 = IRON, 1 = STONE, 0 = IRON/GOLD)
